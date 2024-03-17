d06a3933aca2539848532abcd13a0b031e0a83ad
Siddhesh Bhosale <72059095+siddhesh-coder@users.noreply.github.com>
2024-03-17 19:30:23 +0530

    Delete server directory

--- server/routes/paymentRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { checkout, paymentVerification } from '../controllers/paymentController.js';
-
-const router = express.Router();
-
-router.route('/checkout').post(checkout);
-router.route('/paymentVerification').post(paymentVerification);
-
-export default router;
